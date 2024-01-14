9a452f98c4ffe2848afdc4e4b9f810d93e95bc1d
Stephen Elliott <stephen.elliott231@gmail.com>
2024-01-14 21:29:05 +1100

    Reorganised Header and refactored links

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,21 +3,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function listLink(displayText, to='./' + displayText) {
+function Header() {
   return (
-    <li>
-      <Link to={to} className='text-center fs-5 text-black' style={{ textDecoration: 'none' }}>
-        {displayText}
-      </Link>
-    </li>
-  )
-}
-
-const listSeparator = () => {
-  return (
-    <li className='mx-1 text-secondary'>
-      |
-    </li>
+    <div className='container mt-4 mb-5'>
+      <div className='row align-items-baseline'>
+        <div className='col-6'>
+          {nameBlock()}
+        </div>
+        <div className='col-2' />
+        <div className='col-4'>
+          {listOfLinks()}
+        </div>
+      </div>
+    </div>
   )
 }
 
@@ -35,14 +33,21 @@ const listOfLinks = () => {
   )
 }
 
-const littleCircle = () => {
+const listLink = (displayText, to = './' + displayText) => {
   return (
-    <div className='d-flex'>
-      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='blue'
-        className='bi bi-circle-fill me-3' viewBox='0 0 16 16'>
-        <circle cx='8' cy='8' r='8' />
-      </svg>
-    </div>
+    <li>
+      <Link to={to} className='text-center fs-5 text-black' style={{ textDecoration: 'none' }}>
+        {displayText}
+      </Link>
+    </li>
+  )
+}
+
+const listSeparator = () => {
+  return (
+    <li className='mx-1 text-secondary'>
+      |
+    </li>
   )
 }
 
@@ -58,20 +63,13 @@ const nameBlock = () => {
   )
 }
 
-function Header() {
+const littleCircle = () => {
   return (
-    <div className='container mt-4 mb-5'>
-      <div className='row align-items-baseline'>
-        <div className='col-6'>
-          {nameBlock()}
-        </div>
-
-        <div className='col-2' />
-
-        <div className='col-4'>
-          {listOfLinks()}
-        </div>
-      </div>
+    <div className='d-flex'>
+      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='blue'
+        className='bi bi-circle-fill me-3' viewBox='0 0 16 16'>
+        <circle cx='8' cy='8' r='8' />
+      </svg>
     </div>
   )
 }
