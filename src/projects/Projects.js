912ccf9100624e4698c9a603611447c9b24e8168
Stephen Elliott <stephen.elliott231@gmail.com>
2024-01-29 23:02:35 +1100

    Noted solution to link panel spaghetti code

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -31,18 +31,34 @@ function NavPanel() {
     <div className='container border border-secondary'>
       {/* TODO: fix border size */}
       <div className='mt-3 mb-3'>
-        <h2 className='fs-4'>
-          The Nav Panel
+        <h2 className='fs-2'>
+          Map
         </h2>
       </div>
       <div>
         <div>
-          <NavLinkList subtitle='2024' />
+          <NavLinkList subtitle='' />
         </div>
       </div>
     </div>
   )
 }
+
+// Simple bootstrap alternative to link panel trouble:
+// import React from 'react';
+// import Nav from 'react-bootstrap/Nav';
+
+// function LinksList() {
+//   return (
+//     <Nav className="flex-column flex-sm-row"> {/* Use flexbox to create a 2x1 layout */}
+//       <Nav.Link href="/home">Home</Nav.Link> {/* Use Nav.Link to create a link */}
+//       <Nav.Link href="/about">About</Nav.Link>
+//     </Nav>
+//   );
+// }
+
+// export default LinksList;
+
 
 function NavLinkList({ subtitle }) {
   const links = [
@@ -51,29 +67,30 @@ function NavLinkList({ subtitle }) {
     { to: '/Projects/NextProject', text: 'Next project' }
   ];
 
-  const allLinks =
+  let allLinks =
     links.map((link) =>
-      <NavLinkListItem to={link.to} text={link.text} />);
+      <NavLinkListItem to={link.to} text={link.text} className='col-6'/>);
+
 
   return (
-    <container>
-      <h3 className='fs-5'>
+    <div className=''>
+      <h3 className='fs-4'>
         {subtitle}
       </h3>
-      <ul>
+      <ul className='row list-unstyled align-content-center'>
         {allLinks}
       </ul>
-    </container>
+    </div>
   );
 }
 
 function NavLinkListItem({ to, text }) {
   return (
-    <li>
-      <Link to={to} className='text-center fs-5 text-black' style={{ textDecoration: 'none' }}>
-        {text}
-      </Link>
-    </li>
+      <li className='justify-content-center text-center border border-primary rounded-4 me-3'>
+        <Link to={to} className='p-3 fs-5 text-black' style={{ textDecoration: 'none' }}>
+          {text}
+        </Link>
+      </li>
   )
 }
 
