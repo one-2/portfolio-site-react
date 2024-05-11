0f8b9c7b132dfe80d7874f917319923ba7471440
Stephen Elliott <stephen.elliott231@gmail.com>
2024-05-11 16:00:19 +1000

    Prototype contact page.

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,3 +1,23 @@
+import styles from './page.module.css'
+
+// Heavily inspired by https://codepen.io/jangajdosik/pen/BOmYRm
+// NOTE: rendering workaround for document. bug in the above: https://stackoverflow.com/questions/24647839/referenceerror-document-is-not-defined-in-plain-javascript
 export default function Contact() {
-    return <div>Contact</div>
+  let messageStartString = '> '
+  return (
+    <main className={styles.main}>
+      <div className={styles.terminal}>
+        <div className={styles.header}>
+          <p>contact.</p>
+        </div>
+        <div className={styles.content}>
+        <p className={styles.user}>{messageStartString}run(retrieveContactInformation);</p>
+        <p className={styles.system}>Please input your email address.</p>
+        <p className={styles.user}>{messageStartString}...</p>
+        <p className={styles.system}>Please input your message.</p>
+        <p className={styles.user}>{messageStartString}...</p>
+        </div>
+      </div>
+    </main>
+  )
 }
