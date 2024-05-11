3c6d0676d3ad0085c1dd0c2c9fe71a0bae260d72
Stephen Elliott <stephen.elliott231@gmail.com>
2024-05-11 22:36:59 +1000

    Added user input prototype to the Contact page.

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,28 +1,55 @@
-// The design of this page was heavily inspired by (https://codepen.io/jangajdosik/pen/BOmYRm),
+// The design of this page + children was heavily inspired by (https://codepen.io/jangajdosik/pen/BOmYRm),
 // and some code was borrowed. The program was modified to fit the framework.
+'use client';
 
 import styles from './page.module.css'
+import React, { useEffect, useRef } from 'react';
 
-// TODO: add user input, validation/screening, live text rendering, email sending
-//       or db upload, and this sick matrix background: (https://codepen.io/yaclive/pen/EayLYO)
+// TODO:
+//  - add email string validation.
+//  - add a prompt for the user's message.
+//  - add database upload/email sending with naughty word and attack string validation.
+//  - add a success message / terminal 'deboot'.
+//  - turn the terminal into a modal over the landing page. (https://www.freecodecamp.org/news/how-to-build-a-modal-with-javascript/)
+//  - add this sick matrix background: (https://codepen.io/yaclive/pen/EayLYO)
 
-// NOTE: rendering workaround for document. bug in the above: (https://stackoverflow.com/questions/24647839/referenceerror-document-is-not-defined-in-plain-javascript)
+let messageStartString = 'PS C:\\> '
+
 export default function Contact() {
-  let messageStartString = '> '
   return (
-    <main className={styles.main}>
+    <main className={styles.terminalContainer}>
       <div className={styles.terminal}>
         <div className={styles.header}>
           <p>contact.</p>
         </div>
         <div className={styles.content}>
-        <p className={styles.user}>{messageStartString}run(retrieveContactInformation);</p>
-        <p className={styles.system}>Please input your email address.</p>
-        <p className={styles.user}>{messageStartString}...</p>
-        <p className={styles.system}>Please input your message.</p>
-        <p className={styles.user}>{messageStartString}...</p>
+          <p className={styles.user}>{messageStartString}run(promptForContactInformation)</p>
+          <p className={styles.system}>Please enter your email address.</p>
+          {inputBox()}
+          {/* TODO: Continue user interaction here. */}
         </div>
       </div>
     </main>
+  )
+}
+
+function inputBox() {
+  // TODO: add validation of email and message (validity, no naughty words, no
+  //       attacks.
+
+  // This code focuses the input box when the box is initially rendered.
+  const inputRef = useRef(null);
+  useEffect(() => {
+      inputRef.current.focus();
+  }, []);
+
+  return (
+      <form className={styles.form}>
+          <p className={styles.user}>
+              <label htmlFor='userInput'>{messageStartString}</label>
+              <input className={styles.input} id='userInput' type='text'
+                  ref={inputRef} />
+          </p>
+      </form>
   )
 }
