7479c64aeb34719fd2adbccbfdd857d724aaad02
Stephen Elliott <stephen.elliott231@gmail.com>
2024-06-17 22:00:28 +1000

    Debugged Essays sidebar/backend. Writing backend integration working.

--- app/writing/technicalEssays/layout.js
+++ app/writing/technicalEssays/layout.js
@@ -6,10 +6,31 @@ export const metadata = {
   description: '...',
 }
 
-const EssaysLayout = ({ children }) => {
+async function EssaysLayout({ children }) { // TODO: refactor this and layout in Blogs to one function
+  let data;
+  try {
+    const response = await fetch('http://localhost:5050/writing/essays', {
+      method: 'GET', // Default option, stated for practice
+      headers: {
+        'Content-Type': 'application/json' // Default option, stated for practice
+      }
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new TypeError("Oops, we haven't got JSON!");
+    }
+    data = await response.json();
+  } catch (error) {
+    console.error('Fetch error: ', error);
+  }
+
+  console.log(data)
   return (  
     <div className={styles.main}>
-      <Sidebar rootDataObject={undefined} />
+      <Sidebar rootDataObject={data} />
       {children}
     </div>
   )
